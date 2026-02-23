7ca87e7f9dcbacf008571a004d5685bddde008d7
sandy <sandydwi75@gmail.com>
2026-02-24 03:27:33 +0700

    feat: first commit

---
+++ src/main/java/com/flexpos/pos_dashboard_api/PosDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.flexpos.pos_dashboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosDashboardApiApplication.class, args);
+	}
+
+}
