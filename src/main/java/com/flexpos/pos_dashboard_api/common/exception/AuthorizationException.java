b44826361acb5a665db38a7ac4b4cd8bbf89a18d
sandy <sandydwi75@gmail.com>
2026-03-17 03:59:15 +0700

    feat: update folder structures

--- src/main/java/com/flexpos/pos_dashboard_api/common/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.flexpos.pos_dashboard_api.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class AuthorizationException extends ClientException {
-  public AuthorizationException(String message) {
-    super(message, HttpStatus.FORBIDDEN.value());
-  }
-}
--- src/main/java/com/flexpos/pos_dashboard_api/modules/onboarding/service/OnboardingService.java
+++
@@ -1,9 +0,0 @@
-package com.flexpos.pos_dashboard_api.modules.onboarding.service;
-
-import com.flexpos.pos_dashboard_api.modules.store.entity.Store;
-
-public interface OnboardingService {
-  String getOnboardingStatus();
-  Store getStoreDetail();
-  Store createStore(Store store);
-} 
